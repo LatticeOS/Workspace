1d0f4e60ef17c413d9359ed175038ef08bb2bcab
Gitai <i@gitai.me>
2017-03-08 00:01:08 +0800

    More info of container

--- src/components/workspace/Container.vue
+++ src/components/workspace/Container.vue
@@ -1,20 +1,22 @@
 <template>
-    <md-tabs>
+    <md-tabs class="md-transparent">
         <md-tab id="overview" md-label="Overview">
             <ContainerOverview/>
         </md-tab>
 
         <md-tab id="status" md-label="Status">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
+            <!-- # docker top containerId & docker stats containerId -->
+            <p>Unopened interface</p>
         </md-tab>
 
         <md-tab id="terminal" md-label="Terminal">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
+            <!-- # docker exec containerId -->
+            <p>Unopened interface</p>
         </md-tab>
 
         <md-tab id="fslogs" md-label="File System Changes">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
+            <!-- # docker diff containerId -->
+            <p>Unopened interface</p>
         </md-tab>
     </md-tabs>
 </template>
